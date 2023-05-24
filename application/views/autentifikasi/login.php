e57e8bf2f252bf634b924332e39e3e669f44ff86
rinoramadanrim <rinoramadan.rim@gmail.com>
2023-05-24 14:48:53 +0700

    Pertemuan Ke 9

---
+++ application/views/autentifikasi/login.php
@@ -0,0 +1,49 @@
+	<div class="container">
+
+        <!-- Outer Row -->
+        <div class="row justify-content-center">
+
+            <div class="col-xl-10 col-lg-7 col-md-9">
+
+                <div class="card o-hidden border-0 shadow-lg my-5">
+                    <div class="card-body p-0">
+                        <!-- Nested Row within Card Body -->
+                        <div class="row">
+                            
+                            <div class="col-lg-12">
+                                <div class="p-5">
+                                    <div class="text-center">
+                                        <h1 class="h4 text-gray-900 mb-4">Halaman Login!!</h1>
+                                    </div>
+                                    <form class="user" method="post" action="<?= base_url('autentifikasi'); ?>">
+                                        <div class="form-group">
+                                            <input name="email" type="email" class="form-control form-control-user"
+                                                id="exampleInputEmail" aria-describedby="emailHelp"
+                                                placeholder="Masukan Alamat Email">
+                                        </div>
+                                        <div class="form-group">
+                                            <input name="password" type="password" class="form-control form-control-user"
+                                                id="exampleInputPassword" placeholder="Password">
+                                        </div>
+                                        <a type="submit" class="btn btn-primary btn-user btn-block">
+                                            Masuk
+                                        </a>
+                                    </form>
+                                    <hr>
+                                    <div class="text-center">
+                                        <a class="small" href="forgot-password.html">Lupa Password?</a>
+                                    </div>
+                                    <div class="text-center">
+                                        <a class="small" href="register.html">Daftar Member!</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
